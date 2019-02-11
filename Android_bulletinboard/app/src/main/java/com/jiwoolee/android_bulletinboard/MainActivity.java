d4d62bac64eee4c6211ce44561eb756c3462bfe6
jiwoolee <jnh0465@naver.com>
2019-02-11 22:05:24 +0900

    feat : Connect to Email Login using Firebase's Authentication

--- Android_bulletinboard/app/src/main/java/com/jiwoolee/android_bulletinboard/MainActivity.java
+++ Android_bulletinboard/app/src/main/java/com/jiwoolee/android_bulletinboard/MainActivity.java
@@ -1,146 +1,147 @@
 package com.jiwoolee.android_bulletinboard;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends BaseActivity implements View.OnClickListener {
 
-    private static final String TAG = "GoogleActivity";
-    private static final int RC_SIGN_IN = 9001;
+    private TextView mStatusTextView;
+    private EditText mEmailField;
+    private EditText mPasswordField;
     private FirebaseAuth mAuth;
-    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.signInButton).setOnClickListener(this);
+        mStatusTextView = findViewById(R.id.status); //버튼 참조
+        mEmailField = findViewById(R.id.fieldEmail);
+        mPasswordField = findViewById(R.id.fieldPassword);
+
+        findViewById(R.id.emailSignInButton).setOnClickListener(this);  //리스너 연결
+        findViewById(R.id.emailCreateAccountButton).setOnClickListener(this);
         findViewById(R.id.signOutButton).setOnClickListener(this);
-        findViewById(R.id.disconnectButton).setOnClickListener(this);
 
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
-
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-        mAuth = FirebaseAuth.getInstance();
+        mAuth = FirebaseAuth.getInstance(); //firebase 연결
     }
 
     @Override
-    public void onStart() {
+    public void onStart() { //시작시
         super.onStart();
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        updateUI(currentUser);
+        FirebaseUser currentUser = mAuth.getCurrentUser(); //현재 로그인 되어있는지 확인
+        updateUI(currentUser);                             //로그인 여부에 따라 UI 업데이트
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    private void createAccount(String email, String password) { //이메일계정생성
+        if (!validateForm()) { return; } //폼이 비었으면 return
 
-        if (requestCode == RC_SIGN_IN) {
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            try {
-                GoogleSignInAccount account = task.getResult(ApiException.class);
-                firebaseAuthWithGoogle(account);
-            } catch (ApiException e) {
-                Log.w(TAG, "Google sign in failed", e);
-                updateUI(null);
-            }
-        }
-    }
+        showProgressDialog();
 
-    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
-        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
-
-        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
-        mAuth.signInWithCredential(credential)
+        mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            Log.d(TAG, "signInWithCredential:success");
                             FirebaseUser user = mAuth.getCurrentUser();
                             updateUI(user);
                         } else {
-                            Log.w(TAG, "signInWithCredential:failure", task.getException());
                             updateUI(null);
                         }
+                        hideProgressDialog();
                     }
                 });
     }
 
-    private void signIn() {
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, RC_SIGN_IN);
+    private void signIn(String email, String password) { //로그인
+        if (!validateForm()) {
+            return;
+        }
+
+        showProgressDialog(); //프로그래스
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        //Intent intent=new Intent(MainActivity.this,loginok.class);
+                                        //startActivity(intent);
+                                        FirebaseUser user = mAuth.getCurrentUser();
+                                        updateUI(user);
+                                    } else {
+                                        updateUI(null);
+                        }
+                        if (!task.isSuccessful()) {
+                            mStatusTextView.setText(R.string.auth_failed);
+                        }
+                        hideProgressDialog();
+                    }
+                });
     }
 
-    private void signOut() {
+    private void signOut() { //로그아웃
         mAuth.signOut();
-
-        // Google sign out
-        mGoogleSignInClient.signOut().addOnCompleteListener(this,
-                new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        updateUI(null);
-                    }
-                });
+        updateUI(null);
     }
 
-    private void revokeAccess() {
-        mAuth.signOut();
+    private boolean validateForm() { //로그인 폼
+        boolean valid = true;
 
-        mGoogleSignInClient.revokeAccess().addOnCompleteListener(this,
-                new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        updateUI(null);
-                    }
-                });
-    }
-
-    private void updateUI(FirebaseUser user) {
-        if (user != null) {
-            findViewById(R.id.signInButton).setVisibility(View.GONE);
-            findViewById(R.id.signOutButton).setVisibility(View.VISIBLE);
-            findViewById(R.id.disconnectButton).setVisibility(View.VISIBLE);
+        String email = mEmailField.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            mEmailField.setError("Required.");
+            valid = false;
         } else {
-            findViewById(R.id.signInButton).setVisibility(View.VISIBLE);
-            findViewById(R.id.signOutButton).setVisibility(View.GONE);
-            findViewById(R.id.disconnectButton).setVisibility(View.GONE);
+            mEmailField.setError(null);
+        }
+
+        String password = mPasswordField.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            mPasswordField.setError("Required.");
+            valid = false;
+        } else {
+            mPasswordField.setError(null);
+        }
+
+        return valid;
+    }
+
+    private void updateUI(FirebaseUser user) { //UI 업데이트
+        hideProgressDialog();
+        if (user != null) { //현재 로그인 상태 이면
+            mStatusTextView.setText(getString(R.string.emailpassword_status_fmt,
+                    user.getEmail(), user.isEmailVerified()));
+            findViewById(R.id.loginFields).setVisibility(View.GONE);
+            findViewById(R.id.userFields).setVisibility(View.VISIBLE);
+        } else { //현재 로그인 상태가 아니면
+            mStatusTextView.setText(R.string.signed_out);
+            findViewById(R.id.loginFields).setVisibility(View.VISIBLE);
+            findViewById(R.id.userFields).setVisibility(View.GONE);
         }
     }
 
     @Override
-    public void onClick(View v) {
+    public void onClick(View v) { //버튼클릭시
         int i = v.getId();
-        if (i == R.id.signInButton) {
-            signIn();
-       } else if (i == R.id.signOutButton) {
-           signOut();
-        } else if (i == R.id.disconnectButton) {
-            revokeAccess();
+        if (i == R.id.emailCreateAccountButton) {
+            createAccount(mEmailField.getText().toString(), mPasswordField.getText().toString());
+        } else if (i == R.id.emailSignInButton) {
+            signIn(mEmailField.getText().toString(), mPasswordField.getText().toString());
+        } else if (i == R.id.signOutButton) {
+            signOut();
         }
     }
 }
-
